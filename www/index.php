69858fdbf49ccb9ad10afacd0b93cdf991009f16
James Guinn <jguinn@bonevalleyfilms.com>
2025-07-25 09:26:26 -0400

    Version bump for deploy

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-$version = "2025.07.19";
+$version = "2025.07.25";
 
 // Define debug toggle (hardcoded for now)
 $debug_enabled = isset($_GET['debug']) && $_GET['debug'] === 'true'; // Set to true if ?debug=true is in the URL
