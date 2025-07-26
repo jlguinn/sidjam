09b36f1660b5dbce8941a930b8e2be52455aba26
James Guinn <jguinn@bonevalleyfilms.com>
2025-07-26 10:29:06 -0400

    Repo cleanup

--- jam.cmd
+++
@@ -1,2 +0,0 @@
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito --auto-open-devtools-for-tabs "http://localhost:8080"
-
--- www/tests/test_includ.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once __DIR__ . '/../dbcontrol/sidcon.php';
-echo "host: " . (isset($host) ? $host : 'undefined') . "\n";
-echo "user: " . (isset($user) ? $user : 'undefined') . "\n";
-echo "pass: " . (isset($pass) ? 'defined' : 'undefined') . "\n";
-echo "database: " . (isset($database) ? $database : 'undefined') . "\n";
-?>
